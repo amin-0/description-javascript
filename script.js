4d3d5b8d04d1a148ef241b9f2055bfee5ef2f0aa
Al-amin O <70073642+amin-0@users.noreply.github.com>
2022-06-22 16:15:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = window.prompt('What is your name?')
+var height = window.prompt('How tall are you in centimeters?')
+var country = window.prompt('Which country are you from?')
+
+alert(
+    "Your Name is "+ name+"\n"+
+    "Your height is "+height+" cm"+"\n"+
+    "Your country is "+country
+)
